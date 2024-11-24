9edfd9fb6d262d454452e1756b2e89bbf341c8c4
thofu <thomasfunke71@gmail.com>
2024-11-24 11:31:06 +0100

    Initial Commit

---
+++ Kartenspiel/KartenspielApp.swift
@@ -0,0 +1,17 @@
+//
+//  KartenspielApp.swift
+//  Kartenspiel
+//
+//  Created by Thomas Funke on 24.11.24.
+//
+
+import SwiftUI
+
+@main
+struct KartenspielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
